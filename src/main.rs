cd55a3171e0f73f4cac43cda048012fb80e70535
msakuta <masahiro.sakuta@gmail.com>
2023-09-26 02:51:39 +0900

    An attempt to add wasm build

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,44 @@ mod lander;
 
 use crate::app::LanderApp;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    let native_options = eframe::NativeOptions::default();
+    let mut native_options = eframe::NativeOptions::default();
+
+    // We insist to use light theme, because the canvas color is designed to work with light background.
+    native_options.follow_system_theme = false;
+    native_options.default_theme = eframe::Theme::Light;
+
     eframe::run_native(
         "Lander",
         native_options,
         Box::new(|_cc| Box::new(LanderApp::new())),
     )
     .unwrap();
+}
+
+// when compiling to web using trunk.
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Make sure panics are logged using `console.error`.
+    console_error_panic_hook::set_once();
+
+    // Redirect tracing to console.log and friends:
+    tracing_wasm::set_as_global_default();
+
+    let mut web_options = eframe::WebOptions::default();
+
+    // We insist to use light theme, because the canvas color is designed to work with light background.
+    web_options.follow_system_theme = false;
+    web_options.default_theme = eframe::Theme::Light;
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::start_web(
+            "the_canvas_id", // hardcode it
+            web_options,
+            Box::new(|cc| Box::new(LanderApp::new())),
+        )
+        .await
+        .expect("failed to start eframe");
+    });
 }
