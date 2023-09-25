123f980371980277f7a79b893dbe744244ef3044
msakuta <masahiro.sakuta@gmail.com>
2023-09-25 22:01:29 +0900

    Initial commit (copied from https://github.com/msakuta/rustograd/tree/lander)

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod app;
+mod lander;
+
+use crate::app::LanderApp;
+
+fn main() {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Lander",
+        native_options,
+        Box::new(|_cc| Box::new(LanderApp::new())),
+    )
+    .unwrap();
+}
