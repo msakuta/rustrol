9770d2fadcfe275e81b11eeb77c65ce7b840b425
msakuta <masahiro.sakuta@gmail.com>
2023-10-29 14:27:57 +0900

    Make the view follow the vehicle

--- src/app/bicycle_app.rs
+++ src/app/bicycle_app.rs
@@ -11,11 +11,13 @@ use super::SCALE;
 
 const MAX_THRUST: f64 = 0.5;
 const MAX_STEERING: f64 = std::f64::consts::PI / 4.;
+const STEERING_SPEED: f64 = std::f64::consts::PI * 0.01;
 
 pub struct BicycleApp {
     direct_control: bool,
     paused: bool,
     bicycle: Bicycle,
+    view_offset: Pos2,
     t: f64,
     playback_speed: f64,
     h_thrust: f64,
@@ -28,6 +30,7 @@ impl BicycleApp {
             direct_control: false,
             paused: false,
             bicycle: Bicycle::new(),
+            view_offset: Pos2::ZERO,
             t: 0.,
             playback_speed: 0.5,
             h_thrust: 0.,
@@ -92,8 +95,13 @@ impl BicycleApp {
             );
             let from_screen = to_screen.inverse();
 
-            let canvas_offset_x = response.rect.width() * 0.5;
-            let canvas_offset_y = response.rect.height() * 0.5;
+            let bicycle = &self.bicycle;
+            let view_delta = eframe::emath::vec2(bicycle.pos.x as f32, -bicycle.pos.y as f32)
+                * SCALE
+                - self.view_offset.to_vec2();
+            self.view_offset += view_delta * 0.02;
+            let canvas_offset_x = response.rect.width() * 0.5 - self.view_offset.x;
+            let canvas_offset_y = response.rect.height() * 0.5 - self.view_offset.y;
 
             let to_pos2 = |pos: Vec2<f64>| {
                 to_screen.transform_pos(pos2(
@@ -110,8 +118,31 @@ impl BicycleApp {
                 }
             };
 
-            let bicycle = &self.bicycle;
             let base_pos = to_pos2(bicycle.pos).to_vec2();
+
+            const GRID_SIZE: f64 = 10.;
+            for i in -10..10 {
+                let x = (i as f64 + bicycle.pos.x.div_euclid(GRID_SIZE)) * GRID_SIZE;
+                let tpos = to_pos2(Vec2::new(x, 0.));
+                painter.line_segment(
+                    [
+                        pos2(tpos.x, response.rect.min.y),
+                        pos2(tpos.x, response.rect.max.y),
+                    ],
+                    (1., Color32::GRAY),
+                );
+            }
+            for i in -10..10 {
+                let y = (i as f64 + bicycle.pos.y.div_euclid(GRID_SIZE)) * GRID_SIZE;
+                let tpos = to_pos2(Vec2::new(0., y));
+                painter.line_segment(
+                    [
+                        pos2(response.rect.min.x, tpos.y),
+                        pos2(response.rect.max.x, tpos.y),
+                    ],
+                    (1., Color32::GRAY),
+                );
+            }
 
             painter.add(PathShape::line(
                 bicycle
@@ -133,13 +164,14 @@ impl BicycleApp {
             let rotation = rotation_matrix(bicycle.heading as f32);
             let steering = rotation_matrix((bicycle.heading + bicycle.steering) as f32);
             let rotate_vec = |rotation: &[f32; 4], ofs: &[f32; 2]| {
-                eframe::emath::vec2(
+                [
                     rotation[0] * ofs[0] + rotation[1] * ofs[1],
                     rotation[2] * ofs[0] + rotation[3] * ofs[1],
-                )
+                ]
             };
-            let transform_delta = |ofs: &[f32; 2]| rotate_vec(&rotation, ofs);
-            let transform_vec = |ofs: &[f32; 2]| (transform_delta(ofs) + base_pos).to_pos2();
+            let scale_vec = |scale: f32, vec: &[f32; 2]| [vec[0] * scale, vec[1] * scale];
+            let transform_delta = |ofs: &[f32; 2]| scale_vec(SCALE, &rotate_vec(&rotation, ofs));
+            let transform_vec = |ofs: &[f32; 2]| Pos2::from(transform_delta(ofs)) + base_pos;
             let convert_to_poly = |vertices: &[[f32; 2]]| {
                 PathShape::convex_polygon(
                     vertices.into_iter().map(|ofs| transform_vec(ofs)).collect(),
@@ -149,17 +181,25 @@ impl BicycleApp {
             };
 
             painter.add(convert_to_poly(&[
-                [-10., -10.],
-                [30., -10.],
-                [30., 10.],
-                [-10., 10.],
+                [-2., -2.],
+                [6., -2.],
+                [6., 2.],
+                [-2., 2.],
             ]));
+            painter.add(convert_to_poly(&[[7., -1.], [8., 0.], [7., 1.]]));
 
-            let front_wheel = transform_vec(&[20., 0.]);
-            let front_front = front_wheel + rotate_vec(&steering, &[5., 0.]);
-            let front_back = front_wheel - rotate_vec(&steering, &[5., 0.]);
+            let paint_wheel = |ofs: &[f32; 2], rotation: &[f32; 4]| {
+                let front_wheel = transform_vec(ofs);
+                let front_front =
+                    front_wheel + eframe::emath::Vec2::from(rotate_vec(rotation, &[SCALE, 0.]));
+                let front_back =
+                    front_wheel - eframe::emath::Vec2::from(rotate_vec(rotation, &[SCALE, 0.]));
 
-            painter.line_segment([front_front, front_back], (2., Color32::BLACK));
+                painter.line_segment([front_front, front_back], (2., Color32::BLACK));
+            };
+
+            paint_wheel(&[4., 0.], &steering);
+            paint_wheel(&[0., 0.], &rotation);
         });
     }
 }
@@ -179,7 +219,7 @@ impl Bicycle {
             pos: Vec2::zero(),
             heading: 0.,
             steering: 0.,
-            wheel_base: 20.,
+            wheel_base: 4.,
             pos_history: VecDeque::new(),
         }
     }
@@ -200,7 +240,7 @@ impl Bicycle {
 }
 
 fn bicycle_simulate_step(bicycle: &mut Bicycle, h_thrust: f64, v_thrust: f64, playback_speed: f64) {
-    let steering_speed = playback_speed * 0.1;
+    let steering_speed = playback_speed * STEERING_SPEED;
     bicycle.steering = if (bicycle.steering - h_thrust).abs() < steering_speed {
         h_thrust
     } else if bicycle.steering < h_thrust {
